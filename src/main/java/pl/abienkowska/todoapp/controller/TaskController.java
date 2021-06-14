47b94ade2f716488f317ec69cbfda4b1ab85e064
Agata <agatabienkowska1@wp.pl>
2021-06-14 20:00:53 +0200

    Add columns deadline, created and updated in tasks tabele.

--- src/main/java/pl/abienkowska/todoapp/controller/TaskController.java
+++ src/main/java/pl/abienkowska/todoapp/controller/TaskController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import pl.abienkowska.todoapp.model.Task;
 import pl.abienkowska.todoapp.model.TaskRepository;
@@ -48,13 +49,25 @@ class TaskController {
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
+    @Transactional
     @PutMapping("tasks/{id}")
-    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate ) {
+    public ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate ) {
         if (!repository.existsById(id)) {
             return ResponseEntity.notFound().build();
         }
-        toUpdate.setId(id);
-        repository.save(toUpdate);
+        repository.findById(id)
+                .ifPresent(task -> task.setDone(!task.isDone()));
+        return ResponseEntity.noContent().build();
+    }
+
+    @Transactional
+    @PatchMapping("tasks/{id}")
+    public ResponseEntity<?> toggleTask(@PathVariable int id) {
+        if (!repository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        repository.findById(id)
+                 .ifPresent(task -> task.setDone(!task.isDone()));
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/pl/abienkowska/todoapp/model/Task.java
+++ src/main/java/pl/abienkowska/todoapp/model/Task.java
@@ -1,18 +1,10 @@
 package pl.abienkowska.todoapp.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "tasks")
 public class Task {
 
@@ -22,5 +14,46 @@ public class Task {
     @NotBlank(message = "Tasks description must not be empty")
     private String description;
     private boolean done;
+    private LocalDateTime deadline;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+
+    public Task(){
+    }
+
+    public int getId() {return id;}
+
+    void setId(final int id) {this.id = id;}
+
+    public String getDescription() {return description;}
+
+    void setDescription (final String description) {this.description = description;}
+
+    public boolean isDone() {return done;}
+
+    public void setDone(final boolean done) {
+        this.done = done;
+    }
+
+    public LocalDateTime getDeadline() {return deadline;}
+
+    void setDeadline(final LocalDateTime deadline) { this.deadline = deadline;}
+
+    public void updateFrom (final Task source) {
+        description = source.description;
+        done = source.done;
+        deadline = source.deadline;
+    }
+    @PrePersist
+    void prePersist(){
+        createdOn = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void preMerge(){
+        updatedOn = LocalDateTime.now();
+    }
+
+
 
 }
