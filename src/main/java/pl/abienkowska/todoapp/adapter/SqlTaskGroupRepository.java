8bb2401e8932ba5289a7b76e58fe7080a91318d3
Agata <agatabienkowska1@wp.pl>
2021-06-26 20:29:04 +0200

    Add new migration.

--- src/main/java/pl/abienkowska/todoapp/adapter/SqlTaskGroupRepository.java
+++ src/main/java/pl/abienkowska/todoapp/adapter/SqlTaskGroupRepository.java
@@ -1,12 +1,18 @@
 package pl.abienkowska.todoapp.adapter;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.abienkowska.todoapp.model.TaskGroup;
 import pl.abienkowska.todoapp.model.TaskGroupRepository;
 
+import java.util.List;
+
 @Repository
 interface SqlTaskGroupRepository extends TaskGroupRepository, JpaRepository<TaskGroup, Integer> {
 
-
+    @Override
+    @Query("from TaskGroup g join fetch g.tasks")
+    List<TaskGroup> findAll();
 }
