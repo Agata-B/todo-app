4d032c20419d8fe5efaba7868f43bec602ed8098
Agata <agatabienkowska1@wp.pl>
2021-08-08 12:03:11 +0200

    Refactoring code.

--- src/main/java/pl/abienkowska/todoapp/controller/TaskGroupController.java
+++ src/main/java/pl/abienkowska/todoapp/controller/TaskGroupController.java
@@ -2,7 +2,6 @@ package pl.abienkowska.todoapp.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +30,8 @@ class TaskGroupController {
 
     @PostMapping
     ResponseEntity<GroupReadModel> createGroup(@RequestBody @Valid GroupWriteModel toCreate) {
-        return ResponseEntity.created(URI.create("/")).body(service.createGroup(toCreate));
+        GroupReadModel result = service.createGroup(toCreate);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
     }
 
     @GetMapping
@@ -51,4 +51,13 @@ class TaskGroupController {
         return ResponseEntity.noContent().build();
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
