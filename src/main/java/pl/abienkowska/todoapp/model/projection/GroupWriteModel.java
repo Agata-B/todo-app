ec03734bdde68811b1740d87e0d9185383fc5ab4
Agata <agatabienkowska1@wp.pl>
2021-08-14 16:43:21 +0200

    Refactoring code.

--- src/main/java/pl/abienkowska/todoapp/model/projection/GroupWriteModel.java
+++ src/main/java/pl/abienkowska/todoapp/model/projection/GroupWriteModel.java
@@ -1,5 +1,6 @@
 package pl.abienkowska.todoapp.model.projection;
 
+import pl.abienkowska.todoapp.model.Project;
 import pl.abienkowska.todoapp.model.TaskGroup;
 
 import java.util.Set;
@@ -26,13 +27,14 @@ public class GroupWriteModel {
         this.tasks = tasks;
     }
 
-    public TaskGroup toGroup(){
+    public TaskGroup toGroup(Project project){
         var result = new TaskGroup();
         result.setDescription(description);
         result.setTasks(tasks.stream()
                 .map(source -> source.toTask(result))
                 .collect(Collectors.toSet())
         );
+        result.setProject(project);
         return result;
     }
 }
