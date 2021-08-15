f5fb63e70c32a7fb87ebec0e67d400b5bf635234
Agata <agatabienkowska1@wp.pl>
2021-08-15 23:21:20 +0200

    Add index.html and tasks.html

--- src/main/java/pl/abienkowska/todoapp/controller/ProjectController.java
+++ src/main/java/pl/abienkowska/todoapp/controller/ProjectController.java
@@ -1,18 +1,17 @@
 package pl.abienkowska.todoapp.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.abienkowska.todoapp.logic.ProjectService;
 import pl.abienkowska.todoapp.model.Project;
 import pl.abienkowska.todoapp.model.ProjectStep;
 import pl.abienkowska.todoapp.model.projection.ProjectWriteModel;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Controller
@@ -44,6 +43,7 @@ public class ProjectController {
         }
         service.save(current);
         model.addAttribute("project", new ProjectWriteModel());
+        model.addAttribute("project", getProjects());
         model.addAttribute("message", "Dodano projekt.");
         return "projects";
 
@@ -56,6 +56,21 @@ public class ProjectController {
         return "projects";
     }
 
+    @PostMapping("/{id}")
+    String createGroup(
+            @ModelAttribute("project") ProjectWriteModel current,
+            Model model,
+            @PathVariable int id,
+            @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime deadline
+    ) {
+        try {
+            service.createGroup(deadline, id);
+            model.addAttribute("message", "Dodano grupę.");
+        } catch (IllegalStateException | IllegalArgumentException e) {
+            model.addAttribute("message", "Błąd podczas tworzenia grupy.");
+        }
+        return "projects";
+    }
 
     @ModelAttribute("projects")
     List<Project> getProjects() {
